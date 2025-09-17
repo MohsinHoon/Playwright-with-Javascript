b13e30bd23c86db277bd1dadd0ccb74af633c8b9
Mohsin Ali <162106571+MohsinHoon@users.noreply.github.com>
2025-09-17 19:19:13 +0500

    Delete temp.js

--- temp.js
+++
@@ -1,9 +0,0 @@
-class Animal {
-  constructor(public name: string) {}
-  speak() { return `${this.name} makes a noise`; }
-}
-
-class Dog extends Animal {
-  constructor(name: string) { super(name); }          // call parent constructor
-  speak() { return super.speak() + ' and barks'; }    // reuse + add behavior
-}
